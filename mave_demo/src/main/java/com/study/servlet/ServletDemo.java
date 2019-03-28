8bf475ed1a3e4c030f3c423c2bd03bf62a834347
caolf <308245591@qq.com>
2019-03-28 15:33:44 +0800

    #fix

---
+++ mave_demo/src/main/java/com/study/servlet/ServletDemo.java
@@ -0,0 +1,16 @@
+package com.study.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet(name = "ServletDemo")
+public class ServletDemo extends HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
